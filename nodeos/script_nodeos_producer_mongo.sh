4184275cb2261d415f96ff33b5ad9c21e876c940
trung <trungtt1981@gmail.com>
2018-05-14 16:21:52 +0700

    Correct config for starting producer node

--- nodeos/script_nodeos_producer_mongo.sh
+++ nodeos/script_nodeos_producer_mongo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Name must be eosio for running as block producer
-name=eosio
+name=tttmongo
 base_dir=~/nodeos_data
-config_dir=./nodeos_config
+config_dir=.
 data_dir=$base_dir/$name
 
 nodeos -e -p $name --config-dir $config_dir --data-dir $data_dir \
