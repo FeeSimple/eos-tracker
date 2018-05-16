eadf81bde3de8c56cb8d62d373e4bf64d109e7c5
trung8x <trungtt1981@gmail.com>
2018-05-17 02:03:09 +0700

    Adapt script

--- nodeos/own_nodeos_producer/script_nodeos_producer_mongo.sh
+++ nodeos/own_nodeos_producer/script_nodeos_producer_mongo.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Name must be eosio for running as block producer
-name=eosio
+name=trung
 base_dir=~/nodeos_data
 config_dir=.
 data_dir=$base_dir/$name
 
-nodeos -e -p $name --config-dir $config_dir --data-dir $data_dir \
-  --resync-blockchain --plugin eosio::mongo_db_plugin \
-  --mongodb-uri mongodb://localhost:27017/EOStest
+nodeos --config-dir $config_dir --data-dir $data_dir --resync-blockchain 
+  
