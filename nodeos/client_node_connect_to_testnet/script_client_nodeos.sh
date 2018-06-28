43112ab25834d71422d481c9f179517c93262c6d
trung <trungtt1981@gmail.com>
2018-06-28 22:14:59 +0700

    Update script

--- nodeos/client_node_connect_to_testnet/script_client_nodeos.sh
+++ nodeos/client_node_connect_to_testnet/script_client_nodeos.sh
@@ -1,2 +1,4 @@
-nodeos --genesis-json ./genesis.json --config-dir ./ --data-dir ./ 
-#--delete-all-blocks
+nodeos --genesis-json ./genesis.json --config-dir ./ --data-dir ./ --delete-all-blocks
+# --replay-blockchain 
+# --genesis-json ./genesis.json --config-dir ./ --data-dir ./ --replay-blockchain 
+# --delete-all-blocks
---
+++ nodeos/single_eosio_producer/script_eosio_producer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Name must be eosio for running as block producer
+name=trung
+base_dir=~/nodeos_data
+config_dir=.
+data_dir=$base_dir/$name
+
+nodeos --config-dir $config_dir --data-dir $data_dir --delete-all-blocks 
+  
